c7416b72d1527d3e608746fb9d674f2fab60389d
Martin Hild <martin@nimohi.de>
2025-05-16 12:16:08 +0200

    Aufgabe 13

---
+++ 13_zufallszahl_raten.py
@@ -0,0 +1,41 @@
+import random
+
+MIN = 0
+MAX = 100
+
+def get_user_guess(min_val, max_val):
+    while True:
+        try:
+            user_guess = int(input(f"\nGuess: "))
+            if min_val <= user_guess <= max_val:
+                return user_guess
+            else:
+                print(f"Number must be between {min_val} and {max_val}")
+        except ValueError:
+            print("Must be integer!")
+
+def check_guess(guess, correct):
+    if guess==correct:
+        print("Correct!")
+        return True
+    else:
+        if guess < correct:
+            print("Too low!")
+            return False
+        else:
+            print("Too high!")
+            return False
+
+
+def main():
+    random_number = random.randint(MIN, MAX)
+    print(f"\nGuess number between {MIN} and {MAX}")
+
+    while True:
+        user_guess = get_user_guess(MIN, MAX)
+        if check_guess(user_guess,random_number):
+            break
+
+
+if __name__ == "__main__":
+    main()
