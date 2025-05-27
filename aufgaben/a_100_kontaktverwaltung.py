34e00576b642ed78c2ec9b6390378e8599d7dbd9
martinhild <martin@nimohi.de>
2025-05-27 14:10:43 +0200

    new files, new directory classes

--- aufgaben/a_100_kontaktverwaltung.py
+++ aufgaben/a_100_kontaktverwaltung.py
@@ -4,14 +4,89 @@ Erstelle ein kleines Programm, das Kontakte (Name, Telefonnummer) in einer
 Liste abspeichert und durchsuchen lässt
 """
 
-contacts = []
+from classes.contact_management import Person, Contacts
 
-class Kontakt:
-    def __init__(self, name: str = "", phone_number: str = ""):
-            self.name = name
-            self.phone_number = phone_number
+# Telefonbuch in dem Kontakte gespeichert werden.
+PHONEBOOK = Contacts()
 
-    def add_contact(self, name, phone_number):
-        self.name = name
-        self.phone_number = phone_number
+
+def new_contact():
+    """
+    Fragt nach Name und Telefonnummer und fügt einen
+    neuen Kontakt zum Telefonbuch hinzu.
+    """
+    name = input("Name: ")
+    number = input("Telefonnummer: ")
+    PHONEBOOK.add_contact(name, number)
+    print(f"Hinzugefügt: {name} - {number}")
+
+def search_contact():
+    """
+    Fragt nach Name und Telefonnummer und listet passende Kontakte auf.
+    """
+    name = input("Name: ")
+    print(f"Suchergebnisse für {name}:")
+    print(PHONEBOOK.search_by_name(name))
+
+def delete_contact():
+    """
+    Fragt nach einem Namen und entfernt den  Kontakt nach Bestätigung.
+    """
+    name = input("Name:")
+    PHONEBOOK.remove_contact(name)
+    print(f"{name} wurde gelöscht.")
+
+def print_phonebook():
+    """
+    Zeigt alle Kontakte im Telefonbuch an.
+    """
+    print("--------------------------------------------")
+    print(PHONEBOOK)
+    print("--------------------------------------------")
+
+
+def print_menu():
+    """
+    Zeigt das Hauptmenü mit Optionen an.
+    """
+    print("--------------------------------------------")
+    print("Eingabe: 1 um die Kontakte anzuzeigen.")
+    print("Eingabe: 2 um einen Kontakt hinzuzufügen.")
+    print("Eingabe: 3 um einen Kontakt zu suchen.")
+    print("Eingabe: 4 um einen Kontakt zu löschen ")
+    print("Eingabe: x um zu beenden. ")
+    print("--------------------------------------------")
+
+
+def main():
+    """
+    Steuert die Benutzereingabe und ruft Funktionen auf.
+    """
+    while True:
+        print_menu()
+        user_input = input("Eingabe: ")
+
+        match user_input:
+            case "1":
+                print(PHONEBOOK)
+            case "2":
+                new_contact()
+            case "3":
+                search_contact()
+            case "4":
+                delete_contact()
+            case "x":
+                break
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
 
---
+++ classes/contact_management.py
@@ -0,0 +1,54 @@
+
+class Person:
+    """
+    Person mit Name, Telefonnummer und eindeutiger ID.
+    """
+    _id_counter = 0
+
+    def __init__(self, name: str = "", phone_number: str = ""):
+        Person._id_counter += 1
+        self.id = Person._id_counter
+        self.name = name
+        self.phone_number = phone_number
+
+    def __repr__(self):
+        return f"{self.name} - {self.phone_number}"
+
+    def __str__(self):
+        return f"{self.name} - {self.phone_number}"
+
+
+class Contacts:
+    """
+    Liste von Person-Objekten. Methoden zum Hinzufügen,
+    Entfernen und Suchen von Kontakten.
+    """
+    def __init__(self, contacts: list[Person] = None):
+        if contacts is None:
+            self.contacts = []
+        else:
+            self.contacts = contacts
+
+    def add_contact(self, name, number):
+        new_person = Person(name, number)
+        self.contacts.append(new_person)
+
+    def remove_contact(self, name):
+        for contact in self.contacts:
+            if name == contact.name:
+                if input(f"Remove {contact}? (y/n) ") == "y":
+                    self.contacts.remove(contact)
+
+    def search_by_name(self, name: str):
+        result = []
+        for contact in self.contacts:
+            if name in contact.name:
+                result.append(contact)
+        if len(result) == 0:
+            return "Niemand gefunden."
+        return "\n".join(str(contact) for contact in result)
+
+    def __str__(self):
+        if not self.contacts:
+            return "Keine Kontakte vorhanden."
+        return "\n".join(str(contact) for contact in self.contacts)
